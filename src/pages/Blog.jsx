9185f2c8dae079b3e33b3e871d78946f41e3d4a2
Santi_DMC <sdeveloper.lm@gmail.com>
2025-09-04 17:36:15 -0400

    se agregor Footer en pagina Blog y se modifico el grid

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import NavBar from '../components/UserCompo/NavBar'
 import ArticuloCard from '../components/UserCompo/ArticuloCard'
+import Footer from '../components/UserCompo/Footer'
 function Blog() {
   return (
     <>
@@ -10,15 +11,23 @@ function Blog() {
         <div>
           <h1 >Conoce nuestras ultimas noticias...</h1>
         </div>
-        <div className='row d-flex justify-content-center'>
-          <div className='col'>
-            <ArticuloCard/>
+        {/*aqui va el map del blog*/}
+          <div className='row d-flex justify-content-center py-4'>
+            <div className='col-6'>
+              <ArticuloCard/>
+            </div>
+            <div className='col-6'>
+              <ArticuloCard/>
+            </div>
+            <div className='col-6'>
+              <ArticuloCard/>
+            </div>
+            <div className='col-6'>
+              <ArticuloCard/>
+            </div>
           </div>
-          <div className='col'>
-            <ArticuloCard/>
-          </div>
-        </div>
       </div>
+      <Footer/>
     </>
   )
 }
