e6aea3dec885130f7eaa9b7ec6573124ac0e4247
Santi-DMC <sdeveloper.lm@gmail.com>
2025-09-23 09:28:12 -0300

    copia de seguridad antes de realizar cambios de consumir JSON  a consumir DB

--- src/context/carrito/StateCar.jsx
+++ src/context/carrito/StateCar.jsx
@@ -20,8 +20,9 @@ const StateCar = ({ children }) => {
   });
 
   // traer cupones
-  const listaCupones = useFetch('./ApiCupones');
-
+  const { data: listaCupones} = useFetch('/ApiCupones.json');
+   console.log(listaCupones);
+   
   // recalcular totales
   useEffect(() => {
     let suma = productos.reduce(
@@ -75,11 +76,13 @@ const StateCar = ({ children }) => {
   };
 
   const aplicarCupon = (codigo) => {
-    const cuponValido = listaCupones.find((c) => c.codigo === c.codigo);
+    const cuponValido = listaCupones?.find((c) => c.codigo === codigo);
+
     if (cuponValido) {
-      setCupon(cuponValido);
+      setCupon(cuponValido); // ✅ guardamos el cupón en el estado
       toast.success('Cupón aplicado!');
     } else {
+      setCupon({ codigo: '', descuento: 0 }); // limpiamos si no existe
       toast.error('Cupón inválido');
     }
   };
